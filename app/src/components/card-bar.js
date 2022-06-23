53d95d1842d5325db9dcf9bed18768cc458c5551
Jason Duong <jasontduong@outlook.com>
2022-06-23 00:33:47 -0700

    added placeholder card, cardbar, and dashboard

---
+++ app/src/components/card-bar.js
@@ -0,0 +1,27 @@
+function CardBar() {
+    return (
+    <div class="cardbar">
+        <div class="left">
+            <div class="usericon">
+            </div>
+
+            <ul class="userlink">
+            <li><a href="https://letmegooglethat.com/?q=why+doesn%27t+chagy+pherome">@jasontduong</a></li>
+            </ul>
+        </div>
+
+        <div class="right">
+          <button class="settings">
+            settings
+          </button>
+        
+          <div class="socialicon">
+
+          </div>
+        </div>
+
+    </div>
+    );
+  }
+
+  export default CardBar;
