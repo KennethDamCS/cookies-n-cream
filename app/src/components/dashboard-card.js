bfcbd761ca15571375bd4b80b60d05aaff614574
Jason Duong <jasontduong@outlook.com>
2022-06-23 01:25:17 -0700

    tested card with text

--- app/src/components/dashboard-card.js
+++ app/src/components/dashboard-card.js
@@ -5,6 +5,7 @@ function DashboardCard() {
         <div class="card">
             <CardBar />
               <img class="content" src="https://dskbmzw3v9vmq.cloudfront.net/original/3X/6/f/6f95046379d5055b650fa7afe82de1b527a6042d.jpeg"></img>
+            Vykas release delayed! Confirmed June 30th! We won't be extending the chicken island event or providing additional login bonuses, so please observe booba instead. 
         </div>
 
         <div class="card">
@@ -20,6 +21,7 @@ function DashboardCard() {
         <div class="card">
             <CardBar />
               <img class="content" src="https://images.unsplash.com/photo-1503818454-2a008dc38d43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"></img>
+        
         </div>
             
 
